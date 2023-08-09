75a7b8ac7429de6a5210ed7a0f2ba06e6efd5d6d
Irawo <timothyaremu63@gmail.com>
2023-08-09 15:47:01 +0100

    Add comment input, and item component

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -1,29 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './componets/Button'
+import { comment } from './componets/commentData'
+import CommentItem from './componets/CommentItem'
+import CommentInput from './componets/CommentInput'
 
 const Comment = () => {
+    const [comments, setComments] = useState(comment)
+    const [commentBody, setCommentBody] = useState("")
 
-    const postComment = () => {
+    const addComment = () => {
         try{
-
+            if(!commentBody){
+                throw new Error("Comment field can't be empty")
+            }
+            setComments(oldComments => {
+                return [
+                    {
+                        id: crypto.randomUUID(),
+                        body: commentBody,
+                        comment: []
+                    },
+                    ...oldComments
+                ]
+            })
+            setCommentBody("")
         }
         catch(error){
-            console.log(error)
+            alert(error.message)
         }
     }
 
+    console.log(comments)
   return (
     <div className='comment-container'>
-      <textarea
-        rows={3}
-        placeholder="Add comment"
-      />
-      <br />
-      <Button 
-      text={"Post comment"}
-      handleEvent={postComment}
-      className={"btn-comment"}
-      />
+        {/* Top level comment component */}
+        <h2>React Nested Comment</h2>
+      <CommentInput commentBody={commentBody} setCommentBody={setCommentBody} addComment={addComment} />
+     
+      <div className="users-comment">
+        {comments.map(comment => {
+            return <CommentItem key={comment.id} comment={comment} />
+        })}
+      </div>
     </div>
   )
 }
