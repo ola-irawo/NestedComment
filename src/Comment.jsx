3e20eb9f76b1aa5f7cb3653ac56b16fe4b4becfc
Irawo <timothyaremu63@gmail.com>
2023-12-15 23:27:10 +0100

    fix the error in the nested comment

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -2,48 +2,57 @@ import React, { useState } from 'react'
 import Button from './componets/Button'
 import { comment } from './componets/commentData'
 import CommentItem from './componets/CommentItem'
-import CommentInput from './componets/CommentInput'
-
-const Comment = () => {
-    const [comments, setComments] = useState(comment)
+import CommentInput from './componets/CommentTextarea'
+  
+  const TopLevelComment = () => {
+    const [commentList, setCommentList] = useState([
+      {
+        id: crypto.randomUUID(),
+        body: "This is the first comment",
+        comments: []
+      },
+      {
+        id: crypto.randomUUID(),
+        body: "This is the second comment",
+        comments: []
+      },
+      {
+        id: crypto.randomUUID(),
+        body: "This is the third comment",
+        comments: []
+      },
+    ])
     const [commentBody, setCommentBody] = useState("")
-
-    const addComment = () => {
-        try{
-            if(!commentBody){
-                throw new Error("Comment field can't be empty")
-            }
-            setComments(oldComments => {
-                return [
-                    {
-                        id: crypto.randomUUID(),
-                        body: commentBody,
-                        comment: []
-                    },
-                    ...oldComments
-                ]
-            })
-            setCommentBody("")
-        }
-        catch(error){
-            alert(error.message)
-        }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      setCommentList(prevValue => {
+        return [
+          ...prevValue,
+          {
+            id: crypto.randomUUID(),
+            body: commentBody,
+            comments: []
+          }
+        ]
+      })
+      setCommentBody("")
+      console.log("submit")
     }
-
-    console.log(comments)
-  return (
-    <div className='comment-container'>
-        {/* Top level comment component */}
-        <h2>React Nested Comment</h2>
-      <CommentInput commentBody={commentBody} setCommentBody={setCommentBody} addComment={addComment} />
-     
-      <div className="users-comment">
-        {comments.map(comment => {
+  
+    return (
+      <div className="top-level-comment">
+        <h1>Nested Comment</h1>
+        <CommentTextarea handleSubmit={handleSubmit} commentBody={commentBody}  setCommentBody={setCommentBody} />
+  
+        <br />
+        <div>
+          {commentList.map(comment => {
             return <CommentItem key={comment.id} comment={comment} />
-        })}
+          })}
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
-export default Comment
+  export default TopLevelComment
